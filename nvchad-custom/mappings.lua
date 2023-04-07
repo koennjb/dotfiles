4a07dfd76a20b315dcbf326eb491d197b7ba7b02
K1ueless <kernthekern@gmail.com>
2023-04-07 14:40:33 -0700

    Telescope search current word

--- nvchad-custom/mappings.lua
+++ nvchad-custom/mappings.lua
@@ -2,11 +2,22 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+	},
 }
 
 -- more keybinds!
+
+M.telescope = {
+	n = {
+		["<leader>fw"] = {
+			function()
+				require('telescope.builtin').live_grep({ default_text = vim.fn.expand('<cword>') })
+			end,
+			"find word under cursor",
+		},
+	},
+}
 
 return M
