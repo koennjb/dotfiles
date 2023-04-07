ccdb3196775d5459d7abd56407d7f92a81acfb97
K1ueless <kernthekern@gmail.com>
2023-04-07 13:01:52 -0700

    Track nvchad custom

---
+++ nvchad-custom/init.lua
@@ -0,0 +1,12 @@
+require("custom.autocmds")
+
+local opt = vim.opt
+-- Global options
+opt.showmode = false
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
