16b75c4a6c94aed0ebb8012b92be683097eae9bb
Koenn Becker <8742534+koennjb@users.noreply.github.com>
2025-12-30 19:59:25 -0800

    feat: iterm2 config

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,6 +72,7 @@ PACKAGES=(
     "mise"
     "sketchybar"
     "aerospace"
+    "iterm2"
 )
 
 for package in "${PACKAGES[@]}"; do
--- macos/config.sh
+++ macos/config.sh
@@ -6,3 +6,9 @@ defaults write com.apple.controlcenter "MenuBar" -int 1
 # Install Sbar for Sketchybar
 (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# iterm2 config
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/iterm2-custom"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
